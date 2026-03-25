007bd953d157c75bc98a6d33588c59f3deab947d
Tuan anh Nguyen <ngt4531a@ensfsi.onmicrosoft.com>
2026-03-25 10:23:02 +0100

    [6]LibererEtal fini

--- src/controleur/ControlLibererEtal.java
+++ src/controleur/ControlLibererEtal.java
@@ -23,18 +23,7 @@ public class ControlLibererEtal {
 	 * 		[4] : quantité de produit vendu
 	 */
 	public String[] libererEtal(String nomVendeur) {
-		String[] donneesEtal = new String[5];
-		
-		if(controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur).isEtalOccupe())
-			donneesEtal[0] = "true";
-		else
-			donneesEtal[0] = "false";
-		donneesEtal[1] = nomVendeur;
-		donneesEtal[2] = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur).getProduit();
-		donneesEtal[4] = Integer.toString(controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur).);
-		donneesEtal[3] =
-		
-		return donneesEtal;
+		return controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur).etatEtal();
 	}
 
 }
